1feaa29fd09f8940eef9f61500eee90dfec2a803
Mati <m.m.wasko@student.rug.nl>
2025-05-15 09:48:07 +0200

    Added baseline implementation and edited baseline preprocessing

--- project_name/models/baseline.py
+++ project_name/models/baseline.py
@@ -1,12 +1,41 @@
-from preprocessing.baseline_preprocessing import BaselinePreprocessor
+from project_name.preprocessing import BaselinePreprocessor
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.metrics import classification_report, mean_squared_error, r2_score
 
 
 class BaselineModel:
-    def __init__(self):
-        pass
+
+    def regression(self, regression_type):
+        if regression_type == "logisitc":
+            regression = LogisticRegression(max_iter=1000)
+        else:
+            regression = LinearRegression()
+
+        regression.fit(self.X_training, self.y_training)
+        y_prediction = regression.predict(self.X_test)
+
+        if regression_type == "logisitc":
+            return classification_report(self.y_test, y_prediction)
+        else:
+            mse = mean_squared_error(self.y_test, y_prediction)
+            r2 = r2_score(self.y_test, y_prediction)
+            return mse, r2
 
     def pipeline(self):
-        # do preprocessing and run model, and evaluate
-        preprocesser = BaselinePreprocessor()
-        preprocesser.preprocessing_pipeline()
-        pass
+        preprocesser_tfidf = BaselinePreprocessor(use_tfidf=True)
+        (self.X_training, self.y_training), (self.X_test, self.y_test) =\
+            preprocesser_tfidf.preprocessing_pipeline()
+
+        logistic_results = self.regression(regression_type="logisitc")
+        print(logistic_results)
+
+
+        preprocesser_glove = BaselinePreprocessor(use_tfidf=False)
+        (self.X_training, self.y_training), (self.X_test, self.y_test) =\
+            preprocesser_glove.preprocessing_pipeline()
+        linear_results = self.regression(regression_type="linear")
+        print(linear_results)
+
+if __name__ == "__main__":
+    baseline = BaselineModel()
+    baseline.pipeline()
