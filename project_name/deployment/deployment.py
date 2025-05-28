2bf7672476e9ba80c42124ef8dc7e4946e0bb001
unknown <overgoorlisa@gmail.com>
2025-05-28 17:32:24 +0200

    saved model

---
+++ project_name/deployment/deployment.py
@@ -0,0 +1,6 @@
+
+
+
+# if __name__ == "__main__":
+#     model_loader = ModelSaver()
+#     model = model_loader.load_model("baseline_model")
---
+++ project_name/models/save_load_model.py
@@ -0,0 +1,23 @@
+import pickle
+import os
+
+
+class ModelSaver():
+    """Class to save and load models using pickle."""
+    def __init__(self):
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        self.path = os.path.join(script_dir, "..", "..", "models")
+
+    def save_model(self, model, file_name: str):
+        """Saves the model to a file."""
+        file_path = os.path.join(self.path, file_name)
+        with open(file_path, 'wb') as file:
+            pickle.dump(model, file)
+
+    def load_model(self, file_name: str):
+        """Loads the model from a file."""
+        file_path = os.path.join(self.path, file_name)
+        with open(file_path, 'rb') as file:
+            model = pickle.load(file)
+        print(f"Model loaded from {file_path}")
+        return model
