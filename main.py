088ea3bca89d7b469808f16d6c3c47e57785dadf
Sohret <s.oznadjar@student.rug.nl>
2025-05-29 03:28:18 +0200

    added FastAPI in main.py

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
 from project_name.models.baseline import BaselineModel
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from  project_name.deployment.deployment import predict_emotion
+from starlette.responses import RedirectResponse
 
+app = FastAPI(
+    title="Bert Tweet Sentiment Analysis",
+    summary="An API endpoint to classify emotions of Tweets using Bert "
+)
+
+
+class Input(BaseModel):
+    text: str
+
+
+class Prediction(BaseModel):
+    input: str
+    prediction: str
+
+
+@app.get("/")
+async def root():
+    return RedirectResponse(url='/docs')
+
+
+@app.post("/predict", response_model=Prediction)
+async def predict(input: Input):
+    try:
+        emotion = predict_emotion(input.text)
+    except Exception: 
+        raise HTTPException(status_code=422, detail="validation error")
+    return Prediction(input=input.text, prediction=emotion)
+
+
+"""
 if __name__ == '__main__':
     baseline = BaselineModel()
     print(baseline.pipeline())
+"""
