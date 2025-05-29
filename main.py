dbc6a40d366f2a3de954000a55c86a2ef89041d8
Mati <m.m.wasko@student.rug.nl>
2025-05-29 14:10:09 +0200

    Added Bert Model

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from project_name import BaselineModel
+from project_name.models.bert import BertModel
+from project_name.models.baseline import BaselineModel
 
 if __name__ == '__main__':
-    baseline = BaselineModel()
-    print(baseline.pipeline())
+    type_of_model = "Bert"
+    if type_of_model == "Baseline":
+        baseline = BaselineModel()
+        print(baseline.pipeline())
+    elif type_of_model == "Bert":
+        model = BertModel()
+        model.pipeline()
+        
