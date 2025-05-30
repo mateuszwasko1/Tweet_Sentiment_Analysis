3b3056688e9773abf71fd8de318a23dede17b8f0
Mati <m.m.wasko@student.rug.nl>
2025-05-30 14:59:02 +0200

    Merge Conflict in baseline resolved

--- project_name/preprocessing/baseline_preprocessing.py
+++ project_name/preprocessing/baseline_preprocessing.py
@@ -2,77 +2,25 @@ import re
 import os
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
-<<<<<<< HEAD
-=======
 from project_name.models.save_load_model import ModelSaver
->>>>>>> 487855bbdc533d3eb8fd41d08a927284ce1af3b1
 
 
 class BaselinePreprocessor():
     def __init__(self):
-<<<<<<< HEAD
-        self.test_data = True
-=======
         self.vectorizer = None
         # Stores whether the preprocessor is used for inference or training
         self.at_inference = False
         self.test_data = False
         self.model_saver = ModelSaver()
->>>>>>> 487855bbdc533d3eb8fd41d08a927284ce1af3b1
 
     def extract_features_labels(self, df: pd.DataFrame, feature_name: str,
                                 label_name: str
                                 ) -> tuple[pd.DataFrame, pd.DataFrame]:
-<<<<<<< HEAD
-        # Add value error to check if allowed names are passed on
-=======
->>>>>>> 487855bbdc533d3eb8fd41d08a927284ce1af3b1
         X = df[feature_name]
         y = df[label_name]
         return X, y
 
     def clean_text(self, text: str) -> str:
-<<<<<<< HEAD
-        text = text.lower()
-        text = re.sub(r'@\w+', 'user', text)
-        return text
-
-    def vectorize(self, X: pd.DataFrame):
-        if self.test_data is False:
-            X_vec = self.vectorizer.transform(X)
-        else:
-            self.vectorizer = TfidfVectorizer()
-            X_vec = self.vectorizer.fit_transform(X)
-        return X_vec
-
-    def preprocess_df(self, df: pd.DataFrame):
-        X, y = self.extract_features_labels(df, "tweet", "emotion")
-        X = X.apply(self.clean_text)
-        X_vec = self.vectorize(X)
-        return X_vec, y
-
-    def preprocessing_pipeline(self):
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-        train_path = os.path.join(script_dir, "..", "..", "data", "raw",
-                                  "training_merged.json")
-        dev_path = os.path.join(script_dir, "..", "..", "data", "raw",
-                                "development_merged.json")
-        test_path = os.path.join(script_dir, "..", "..", "data", "raw",
-                                 "test-gold_merged.json")
-        training_data = pd.read_json(train_path, orient="records", lines=True)
-        dev_data = pd.read_json(dev_path,
-                                orient="records", lines=True)
-        test_data = pd.read_json(test_path,
-                                 orient="records", lines=True)
-        X_training, y_training = self.preprocess_df(pd.concat([training_data,
-                                                               dev_data]))
-        self.test_data = False
-        # Either delete line below or dont concat
-        # X_dev, y_dev = self.preprocess_df(dev_data)
-        X_test, y_test = self.preprocess_df(test_data)
-        # return (X_training, y_training), (X_dev, y_dev), (X_test, y_test)
-        return (X_training, y_training), (X_test, y_test)
-=======
         if not isinstance(text, str):
             raise ValueError("Input must be a string.")
         text = text.lower()
@@ -132,23 +80,13 @@ class BaselinePreprocessor():
                 raise TypeError("Data must be a pandas DataFrame.")
             preprocessed_df = self.preprocess_df(data)
             return preprocessed_df
->>>>>>> 487855bbdc533d3eb8fd41d08a927284ce1af3b1
 
 
 if __name__ == "__main__":
     preprocessor = BaselinePreprocessor()
-<<<<<<< HEAD
-    # ((X_training, y_training), (X_dev, y_dev),
-    # (X_test, y_test)) = preprocessor.preprocessing_pipeline()
-    ((X_training, y_training),
-     (X_test, y_test)) = preprocessor.preprocessing_pipeline()
-    print(X_training, y_training)
-    # print(X_dev, y_dev)
-=======
     (X_training, y_training), (X_dev, y_dev), (X_test, y_test) = preprocessor.preprocessing_pipeline()
     (X_training, y_training), (X_test, y_test) = preprocessor.preprocessing_pipeline()
     print(X_training, y_training)
     print(X_dev, y_dev)
 
->>>>>>> 487855bbdc533d3eb8fd41d08a927284ce1af3b1
     print(X_test, y_test)
