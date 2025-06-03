b350a7eea11fa570abd533dc56f4378029e9d161
unknown <overgoorlisa@gmail.com>
2025-06-03 17:52:23 +0200

    changed baseline_preprocessing

--- project_name/deployment/fastAPI.py
+++ project_name/deployment/fastAPI.py
@@ -1,6 +1,3 @@
-from project_name.models.bert_ekphrasis import BertModel
-from project_name.models.baseline import BaselineModel
-from project_name.models.prediction_bert_ekphrasis import PredictEkphrasisBert
 from fastapi import FastAPI, HTTPException, Body
 from pydantic import BaseModel
 from project_name.deployment.process_deployment import predict_emotion
@@ -110,30 +107,3 @@ async def predict(input_data: List[Input] = Body(...)):
             raise HTTPException(status_code=422, detail="validation error")
         results.append(Prediction(input=item.text, prediction=emotion))
     return results
-
-
-if __name__ == '__main__':
-    type_of_model = "Bert_p"
-    if type_of_model == "Baseline":
-        baseline = BaselineModel()
-        print(baseline.pipeline())
-    elif type_of_model == "Bert":
-        model = BertModel()
-        model.pipeline()
-    elif type_of_model == "Bert_p":
-        prediction = PredictEkphrasisBert()
-        number_of_predictions = int(input("How many predictions would you like\
-        to make?"))
-        if number_of_predictions <= 0:
-            raise ValueError("Number of predictions must be greater than 0.")
-        i = 0
-        while i < number_of_predictions:
-            i += 1
-            text = input("What text would you like predict?")
-            label_class, prob = prediction.predict(text)
-            print(f"The predicted class is {label_class} with a probability of\
-                   {(prob*100):.2f}%.")
-    baseline = BaselineModel()
-    baseline_metrics = baseline.pipeline()
-    print(baseline.best_parameters)
-    print(baseline_metrics)
